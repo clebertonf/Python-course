819368b8c74ed20b9e70590cd2a0421243af2a25
Cleberton Francisco <clebertonfgc@gmail.com>
2021-08-17 18:29:18 -0300

    Finalizando aula operadores logicos

---
+++ 001-Python-basico/012-condicionais-operadores-logicos.py
@@ -0,0 +1,44 @@
+# and, or, not, in, not in
+
+num_1 = 2
+num_2 = 2
+num_3 = 5
+
+# and as duas condições devem ser verdadeiras par retornar True
+if num_1 == num_2 and num_2 < num_3:
+    print(True)
+else:
+    print(False)
+
+# or somente uma condição deve ser verdadeira para retornar True
+if num_1 == num_2 or num_2 > num_3:
+    print(True)
+else:
+    print(False)
+
+# not inverte a expressão de True para false ou vice e versa
+if not num_1 == num_2 or not num_2 < num_3:
+    print(True)
+else:
+    print(False)
+
+value = ''
+
+if not value:
+    print('Preencha o campo!')
+
+# in (esta contido)
+
+nome = 'Cleberton'
+
+if 'l' in nome:
+    print('A letra está presente!')
+else:
+    print('Não existe')
+
+# not in (nao esta contido)
+
+if 'l' not in nome:
+    print('A letra naão esta presente!')
+else:
+    print('a letra existe')
