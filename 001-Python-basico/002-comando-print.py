eacc3e0c2739b3724103fb8220f435f402d53d88
Cleberton Francisco <clebertonfgc@gmail.com>
2021-08-17 12:05:42 -0300

    Função print finalizada

---
+++ 001-Python-basico/002-comando-print.py
@@ -0,0 +1,12 @@
+print('Olá mundo!')
+
+# A função print recebe um argumento, ou mais.
+
+print('Tudo', 'otimo', '?')
+
+# Argumentos nomeados
+
+print('Cleberton', 'Francisco', sep='-')  # separador sera -
+
+print('Esta', 'Tudo', 'bem? ', end='...')  # adiciona algo no final da string
+
