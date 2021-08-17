4fb6efbc447be15498dd26d3283245c51dc8cce6
Cleberton Francisco <clebertonfgc@gmail.com>
2021-08-17 16:41:30 -0300

    iniciando estudo entrada de dados

---
+++ 001-Python-basico/009-entrada-de-dados.py
@@ -0,0 +1,13 @@
+# entrada de dados em python
+
+valor = input('Digite sue nome: ')
+print(valor)
+
+numer_1 = input('Digite um numero: ')
+numer_2 = input('Digite outro numero: ')
+
+
+numer_1_int = int(numer_1)
+numer_2_int = int(numer_2)
+
+print(f'A soma é: {numer_1_int + numer_2_int}')
