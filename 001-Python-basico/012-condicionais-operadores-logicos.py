f75429744dac9dea5b8499e7adaa7d3b39f7695a
Cleberton Francisco <clebertonfgc@gmail.com>
2021-08-17 18:51:01 -0300

    Finalizando aula operadores logicos

--- 001-Python-basico/012-condicionais-operadores-logicos.py
+++ 001-Python-basico/012-condicionais-operadores-logicos.py
@@ -42,3 +42,13 @@ if 'l' not in nome:
     print('A letra naão esta presente!')
 else:
     print('a letra existe')
+
+
+num_1 = 0
+num_2 = 0
+ 
+if not num_1 != num_2:
+    print('Retorno 1')
+else:
+    print('Retorno 2')
+    
---
+++ 001-Python-basico/tempCodeRunnerFile.py
@@ -0,0 +1,8 @@
+
+num_1 = 0
+num_2 = 0
+ 
+if not num_1 != num_2:
+    print('Retorno 1')
+else:
+    print('Retorno 2')
