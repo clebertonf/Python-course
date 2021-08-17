59f48f0a4fa66e935c3fa006f4cf563f7b685f62
Cleberton Francisco <clebertonfgc@gmail.com>
2021-08-17 13:48:59 -0300

    Operadores aritmeticos

---
+++ 001-Python-basico/006-operadores-aritmeticos.py
@@ -0,0 +1,24 @@
+# Operadores aritméticos
+
+"""
+    + soma
+    - subtração
+    * multiplicação
+    / divisão
+    // divisão inteira
+    ** exponenciaçãopresce
+    % resto da divisão
+    () precedência
+"""
+
+print(10 + 10)
+print(10 - 5)
+print(10 * 3)
+print(10 / 2)
+print(10 // 2)
+print(10 ** 2)
+print(10 % 2)
+
+print((10 + 2) * 3)  # Parênteses tem prioridade
+
+print(10 * 'Cleber - ')  # Imprime a string (Cleber) 10 vezes
