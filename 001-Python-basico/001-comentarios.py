566c0b992b993ba46b516b0a69a5ff5102f35c52
Cleberton Francisco <clebertonfgc@gmail.com>
2021-08-16 23:32:45 -0300

    Finaliza aula de comentarios

--- 001-Python-basico/001-comentarios.py
+++ 001-Python-basico/001-comentarios.py
@@ -7,3 +7,6 @@ print("Aqui estou printando um texto")
     Doc Strings
     são cometarios de multiplas linhas.
 """
+# Códigos entre """ e """ são usados para documentação e, 
+# geralmente, não são executados,
+#  por este motivo, podem ser usados como "comentários".
