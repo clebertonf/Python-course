f0b5485a8439c1739beb0419d69b0a5e82726a8f
Cleberton Francisco <clebertonfgc@gmail.com>
2021-08-17 12:26:42 -0300

    aula strings finalizada

---
+++ 001-Python-basico/003-strings.py
@@ -0,0 +1,12 @@
+# tipo str no python
+# Para cadeias de texto
+
+nome = "Cleberton Francisco"
+print(nome)
+
+print("Isso é uma 'string'")
+
+# caracter de scape
+# Alguns casos será necessário o uso
+
+print("Isso é uma \"string\"")
