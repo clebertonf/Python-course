a066428e7245d836f0635e7f45dbe762d2262318
Cleberton Francisco <clebertonfgc@gmail.com>
2021-08-17 16:54:09 -0300

    Finalizando input dados python

--- 001-Python-basico/009-entrada-de-dados.py
+++ 001-Python-basico/009-entrada-de-dados.py
@@ -3,11 +3,7 @@
 valor = input('Digite sue nome: ')
 print(valor)
 
-numer_1 = input('Digite um numero: ')
-numer_2 = input('Digite outro numero: ')
+numer_1 = int(input('Digite um numero: '))
+numer_2 = int(input('Digite outro numero: '))
 
-
-numer_1_int = int(numer_1)
-numer_2_int = int(numer_2)
-
-print(f'A soma é: {numer_1_int + numer_2_int}')
+print(f'A soma é: {numer_1 + numer_2}')
---
+++ 001-Python-basico/010-condicionais-boleanos.py
@@ -0,0 +1,19 @@
+# if, elif e else
+
+# exemplos basico de uso
+
+number = 1
+
+if number == 1:
+    print(f'É igual a {number}')
+elif number == 2:
+    print(f'É igual a {number}')
+else:
+    print('Não identifiquei o numero!')
+
+bol = True
+
+if bol:
+    print('True')
+else:
+    print('False')
