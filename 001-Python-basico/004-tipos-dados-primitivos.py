33949e873f5c78b65f5fabe194b6147b17b74355
Cleberton Francisco <clebertonfgc@gmail.com>
2021-08-17 13:33:40 -0300

    Tipos primitivos finalizando

--- 001-Python-basico/004-tipos-dados-primitivos.py
+++ 001-Python-basico/004-tipos-dados-primitivos.py
@@ -2,7 +2,7 @@
 # Python é uma linguagem de tipagem dinâmica
 
 """
-    str - string (Cadeis de texto)
+    str - string (Cadeias de texto)
     int - inteiro (numeros inteiros)
     float - real / ponto flutuante (mumeros com casas decimais (1.5, 10.10))
     bool - booleano / lógico (True / False)
@@ -16,4 +16,12 @@ print(type(nome))  # <class 'str'>
 print(type(idade))  # <class 'int'>
 print(type(nota))  # <class 'float'>
 print(type(ativo))  # <class 'bool'>
+
+print('###################')
+# Converter tipos
+
+idade = 27
+print(type(idade))
+nova_idade = str(idade)  # convertendo para str
+print(type(nova_idade))
 
