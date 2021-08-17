5af41fb3f26c50ae1d24127e57d9d58a0547d465
Cleberton Francisco <clebertonfgc@gmail.com>
2021-08-17 16:12:35 -0300

    Finalizando desafio, add comentario sobre a função

--- 001-Python-basico/008-desafio-pratico.py
+++ 001-Python-basico/008-desafio-pratico.py
@@ -6,9 +6,12 @@ year_current_date = date.today().year
 def get_info(name, age, height, weight):
     year_birth = year_current_date - age
     imc = round(weight / (height ** 2), 2)
-    print(f'{name} tem {age} anos, {height} de altura e pesa {weight} KG.')
-    print(f'O IMC do {name} é: {imc}')
-    print(f'{name} nasceu em {year_birth}')
+    print(f"{name} tem {age} anos, {height} de altura e pesa {weight} KG.")
+    print(f"O IMC do {name} é: {imc}")
+    print(f"{name} nasceu em {year_birth}")
 
 
-get_info('Cleberton', 28, 1.69, 75)
+get_info("Cleberton", 28, 1.69, 75)
+
+# Função recebe algumas informaçoes por parametro, e retorna ano de nascimento, imc
+# com algumas frases customizadas
