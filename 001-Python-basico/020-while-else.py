a6c405e2f0e468105ec969ff1f69f8cedfb704c6
Cleberton Francisco <clebertonfgc@gmail.com>
2021-08-18 13:58:24 -0300

    Finalizando estudo while /  else

---
+++ 001-Python-basico/020-while-else.py
@@ -0,0 +1,19 @@
+# While / else
+
+# Acumulador
+
+contador = 0
+acumulador = 0
+
+while contador <= 10:
+    print(contador)
+    acumulador += contador  # soma de todo contador em cada iteração
+
+    contador += 1
+    print(acumulador)
+else:
+    print('Saiu do laço')
+
+# Em python o while possui um else que é executado no final do laço
+# else nao e executado se a condição do while passar a ser falsa
+# só entra no else caso o laço inteiro for percorrido
--- 001-Python-basico/tempCodeRunnerFile.py
+++
@@ -1,6 +0,0 @@
-
-numero_2 = 0
-
-while numero <= 5:
-    print(numero)
-    numero += 1
