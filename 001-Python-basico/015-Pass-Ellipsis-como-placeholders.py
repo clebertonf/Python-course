483af48091b98d0f5bea313f7d010a4fa3e627bc
Cleberton Francisco <clebertonfgc@gmail.com>
2021-08-17 23:51:55 -0300

    Pass-Ellipsis-como-placeholders

---
+++ 001-Python-basico/015-Pass-Ellipsis-como-placeholders.py
@@ -0,0 +1,12 @@
+# 015-Pass-Ellipsis-como-placeholders
+
+# deixar trechos de codigos em aberto para implementação posterior
+
+active = False
+
+if active:
+    pass  # aqui falo para passar direto, pois não tenho codigo implementado
+elif active:
+    ...  # Ellipsis faz a mesma coisa do pass
+else:
+    print('Ola')
