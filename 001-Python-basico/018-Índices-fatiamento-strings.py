152b931b46e1588d91b362c7ca8f2329aa0e3bd9
Cleberton Francisco <clebertonfgc@gmail.com>
2021-08-18 11:30:58 -0300

    indices em strings

---
+++ 001-Python-basico/018-Índices-fatiamento-strings.py
@@ -0,0 +1,31 @@
+# Manipulação de strings
+
+"""
+    Indices de strings
+    Fatiamento [inicio:fim:passo]
+    Funções built-in
+
+    Referêcia para estudo:
+     Built-in Types¶: https://docs.python.org/3/library/stdtypes.html
+     Built-in Functions¶: https://docs.python.org/3/library/functions.html
+"""
+# Indices de strings
+
+url = 'www.google.com.br/'
+print(url[1])  # w
+print(url[-1])  # /
+
+# Fatiamento
+
+print(url[0:3])  # www
+print(url[4:10])  # google
+
+print(url[::2])  # wwgol.o.r (passo de 2 em 2)
+print(url[::-2])  # /bmcego.w (passso de 2 em 2 de tras para frente)
+
+print(url[0:10])  # www.google
+
+
+# Funções built-in
+
+print(url.replace('/', ''))
