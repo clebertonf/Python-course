b04a02067473120c9328eb471311ab72d78a2f7c
Cleberton Francisco <clebertonfgc@gmail.com>
2021-08-19 22:32:30 -0300

    Append, pop, extend, insert

--- 001-Python-basico/023-listas.py
+++ 001-Python-basico/023-listas.py
@@ -10,3 +10,35 @@ lista = [1, 2, 3, 4]
 lista_2 = ['Cleber', True, 10]
 
 print(lista, lista_2)
+
+# modificando lista
+lista[3] = 6
+print(lista)
+
+# percorrendo lista
+print(lista[0:3])
+
+# append (insere valor ao final)
+lista_3 = ['a', 'b', 'c']
+lista_3.append('d')
+
+
+# extend (junta duas listas)
+
+lista_4 = ['e', 'f', 'g']
+
+lista_3.extend(lista_4)
+
+print(lista_3)
+
+# insert (escolhe o indice que ira inserir)
+
+lista_3.insert(3, 'J')
+print(lista_3)
+
+
+# pop (remove o ultimo valor da lista)
+
+lista_4.pop()
+print(lista_4) # G removidp
+
