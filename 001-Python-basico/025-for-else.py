5e1bf7cf0102ca48789ce07e3b9f6e7c9c3bce58
Cleberton Francisco <clebertonfgc@gmail.com>
2021-08-20 11:39:40 -0300

    For else em python

---
+++ 001-Python-basico/025-for-else.py
@@ -0,0 +1,12 @@
+# For else
+
+nomes = ['Cleber', 'Francisco', 'Katrine']
+
+for nome in nomes:
+    if nome.lower().startswith('c'):
+        print('Achei a letra')
+        break
+else:
+    print('Acabou laço')
+
+# else no for é executado apos o laço terminar, caso a condição do if seja false
