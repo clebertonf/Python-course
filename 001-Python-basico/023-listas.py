614e4e49e211e2e1bfe08d64153c50a8a04a4a1c
Cleberton Francisco <clebertonfgc@gmail.com>
2021-08-19 22:09:04 -0300

    Iniciando listas em python

---
+++ 001-Python-basico/023-listas.py
@@ -0,0 +1,12 @@
+"""
+    Listas
+    Fatiamento
+    append, insert, pop, del, clear, extend, +
+    min, maz
+    range
+"""
+
+lista = [1, 2, 3, 4]
+lista_2 = ['Cleber', True, 10]
+
+print(lista, lista_2)
