60ec98188aac5f89998001cd86b481d9c645b398
Cleberton Francisco <clebertonfgc@gmail.com>
2021-08-30 16:16:46 -0300

    Explicações enumerate, add cometario

--- 001-Python-basico/027-enumerate.py
+++ 001-Python-basico/027-enumerate.py
@@ -1,6 +1,6 @@
 list_names = ["Cleber", "Katrine", "Tody", "luna", "Jake"]
 
-for numero, item_lista in enumerate(list_names):
+for numero, item_lista in enumerate(list_names, 5): # posso passar valor inicial
     print(numero, item_lista)
 
 # Enumerate, enurema os itens de uma lista que é passado como parâmetro
