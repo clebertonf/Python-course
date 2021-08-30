0e2e530a184cc07644b6cce668aab8ae1dca257f
Cleberton Francisco <clebertonfgc@gmail.com>
2021-08-30 16:15:19 -0300

    Explicações enumerate

---
+++ 001-Python-basico/027-enumerate.py
@@ -0,0 +1,6 @@
+list_names = ["Cleber", "Katrine", "Tody", "luna", "Jake"]
+
+for numero, item_lista in enumerate(list_names):
+    print(numero, item_lista)
+
+# Enumerate, enurema os itens de uma lista que é passado como parâmetro
